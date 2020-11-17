fde2f275d44825a8af504ac23bae8fd602e486cc
Sven Kellenberger <kellenberger@ubique.ch>
2020-11-17 19:34:52 +0100

    fix prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = env => {
-    merge(common(env), {
+    return merge(common(env), {
         mode: 'production',
     });
 }
