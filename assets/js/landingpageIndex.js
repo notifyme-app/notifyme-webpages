40610b0f6211aa99efa4bcd0d46ea5f2f8263966
Sven Kellenberger <kellenberger@ubique.ch>
2020-11-18 17:33:57 +0100

    change link location

--- assets/js/landingpageIndex.js
+++ assets/js/landingpageIndex.js
@@ -4,17 +4,17 @@ ready(() => console.log(`Commit: ${GIT_INFO}`));
 
 ready(() => {
     if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-        if(window.storeLinks.playStore === "https://example.com") {
+        if(window.currentLanguage.playStoreLink === "https://example.com") {
             console.log("would redirect android");
             return;
         }
-        window.location.href = window.storeLinks.playStore;
+        window.location.href = window.currentLanguage.playStoreLink;
     }
     if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
-        if(window.storeLinks.appStore === "https://example.com") {
+        if(window.currentLanguage.appStoreLink === "https://example.com") {
             console.log("would redirect iphone");
             return;
         }
-        window.location.href = window.storeLinks.appStore;
+        window.location.href = window.currentLanguage.appStoreLink;
     }
 });
