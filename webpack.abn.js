6d94faff3ac9861a2304faa3ceb4dea1ec6bf830
alig <alig@ubique.ch>
2020-12-15 10:36:58 +0100

    add abn env

---
+++ webpack.abn.js
@@ -0,0 +1,12 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = (env) => {
+    return merge(common(env), {
+        mode: "development",
+        devtool: "inline-source-map",
+        optimization: {
+            minimize: false,
+        },
+    });
+};
